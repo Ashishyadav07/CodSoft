c8abfcae106f683c657116efce66a0d6a44a87ea
Ashishyadav07 <ashishyadav25800@gmail.com>
2023-12-19 18:59:17 +0530

    Implement MongoDB connection and user registration
    1. Add a MongoDB connection function to establish a connection to the database.
    2. Create a user model with necessary fields using Mongoose.
    3. Implement user registration functionality with validation and password hashing.
    4. Add authentication routes for user registration.
    
    Details:
    - config/db.js : Added a function to connect to MongoDB.
    - controllers/authController.js : Implemented user registration logic with validation and password hashing.
    -  helpers/authHelper.js : Created functions for password hashing and password comparison.
    -  models/userModels.js : Defined a Mongoose schema for the user model.
    - routes/authRoute.js : Created authentication routes for user registration.
    - server.js : Configured the Express app, set up middleware, and defined routes.

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
+
+//configure env
+dotenv.config();
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+//routes
+app.use('/api/v1/auth', authRoutes);
+
+//rest api 
+app.get('/', (req, res) => {
+    res.send("<h1>Weclome to my Shoe Website</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
